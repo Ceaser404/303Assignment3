008c188243a5b38af709956ed750b38172fe586b
Ceaser404 <119560473+Ceaser404@users.noreply.github.com>
2023-04-12 00:09:03 -0500

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -8,15 +8,20 @@ int main() {
     cout <<
         "Enter expressions to be converted, or press Return when done\n";
     string expression;
+    // gather a line of input from the user using getline
     while (getline(cin, expression)) {
+        // handle exception of no input
         if (expression == "") break;
+        // call member function .isBalanced, which returns a bool
         try {
             if(infix_to_postfix.isBalanced(expression))
                 cout << "Parentheses are balanced\n";
             else
                 cout << "Expression is invalid\n";
+            // convert expression from infix to postfix and output to user
             string result = infix_to_postfix.convert(expression);
             cout << "== " << result << endl;
+            break;
         } catch (char* ex) {
             cout << "Syntax Error: " << endl;
         }

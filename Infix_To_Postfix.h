b09d6f78ab65d77591cc489241a54bba16c12a52
Ceaser404 <119560473+Ceaser404@users.noreply.github.com>
2023-04-12 00:10:02 -0500

    Update Infix_To_Postfix.h

--- Infix_To_Postfix.h
+++ Infix_To_Postfix.h
@@ -13,23 +13,27 @@ using namespace std;
 
 class Infix_To_Postfix {
     public:
+        // converts function from infix to postfix
         string convert(const string& expression);
+        // checks if expression parenthesis are balanced
         bool isBalanced(const string& expression);
     private:
+        // processes the operators and parenthesis for convert function
         void process_operator(char op);
-        
+        // checks if character is an operator
         bool is_operator(char ch) const {
-            cout << "operators" << endl;
             return OPERATORS.find(ch) != string::npos;
         }
-        
+        // checks the precedence of operators to decide whether to push to stack
         int precedence(char op) const {
             return PRECEDENCE[OPERATORS.find(op)];
         }
-        
+        // string of characters that we consider operators
         static const string OPERATORS;
+        // keeps track of the order of prededence of the operators
         static const int PRECEDENCE[];
         #ifdef USEKW
+        // define data type for operator_stack
         KW::stack<char> operator_stack;
         #else
         stack<char> operator_stack;
